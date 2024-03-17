24d445c5400ec2ab426f794fdf511e4a300e1f4d
thor <sahilarora8527397612@gmail.com>
2024-03-17 10:16:22 +0530

    made otp box

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import VerifyHost from './views/Customer/VerifyHost';
+import ViewBill from './views/Customer/ViewBill';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+        <Route path="/bill" element={<ViewBill />} />
           <Route path="/" element={<VerifyHost />} />
         </Routes>
       </BrowserRouter>
