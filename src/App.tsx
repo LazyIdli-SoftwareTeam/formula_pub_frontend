0a31e01bc261f03e7fb10cdaff1e9852281492b2
notadevps <sahilarora8527397612@gmail.com>
2024-03-19 19:21:12 +0530

    added links with only branch id and event id

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import {
   BrowserRouter,
   Routes,
   Route,
-  useSearchParams,
 } from 'react-router-dom';
 import VerifyHost from './views/Customer/VerifyHost';
 import ViewBill from './views/Customer/ViewBill';
@@ -13,16 +12,20 @@ import GenerateRacePass from './views/Customer/GenerateRacePass';
 import Outlay from './Outlay';
 
 function App() {
-  const [searchParams] = useSearchParams();
-  const eventId = searchParams.get('eventId');
-  const branchId = searchParams.get('branchId');
-  if (!eventId || !branchId) {
-    return (
-      <div>
-        <span>Event id or branch id is required</span>
-      </div>
-    );
-  }
+  // const [searchParams, setSearchParams] = useSearchParams();
+  // const eventId = searchParams.get('eventId');
+  // const branchId = searchParams.get('branchId');
+  // console.log(eventId);
+  // console.log(branchId);
+  // if (!eventId || !branchId) {
+  //   return (
+  //     <div>
+  //       <span style={{ color: 'white', fontSize: '30px' }}>
+  //         Event id or branch id is required
+  //       </span>
+  //     </div>
+  //   );
+  // }
   return (
     <>
       <BrowserRouter>
